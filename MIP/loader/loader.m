b3c774cc6dfd24a42c1671fb038fc1b406f7e6b6
Banny <zhkl0228@qq.com>
2022-10-16 04:25:12 +0800

    Replace lsdinjector.dylib with loader.dylib and remove obsoleted i386.

--- MIP/loader/loader.m
+++ MIP/loader/loader.m
@@ -1,11 +1,11 @@
 #include <Foundation/Foundation.h>
-#include <unistd.h>
-#include <stdio.h>
 #include <mach-o/dyld.h>
-#include <sys/syslimits.h>
-#include <dlfcn.h>
+#include <bsm/libbsm.h>
+#include <libproc.h>
+#include <pwd.h>
 
 #include "loader.h"
+#include "injector/inject/inject.h"
 
 #define objc_collectingEnabled() (@selector(retain) == @selector(release))
 
@@ -18,6 +18,84 @@ const char *MIP_user_data_path(void)
         sprintf(user_data_path, USER_DATA_ROOT "/%d", getuid());
     }
     return user_data_path;
+}
+
+// Private APIs
+extern mach_port_t xpc_dictionary_copy_mach_send(xpc_object_t, const char *);
+extern void xpc_dictionary_get_audit_token(xpc_object_t xdict, audit_token_t *token);
+
+/* We want the user data to be accessible from all processes, but some processes (for
+   example, Chrome's sub-processes) have a very strict sandbox profile so putting the
+   data in the user's home folder won't always work. /usr/lib, on the other hand, is
+   accessible from all processes (Otherwise they're pretty much worthless to hook in
+   the first place), so we put our data in /usr/lib/mip/user_data/<uid>/. We also set
+   a symlink in the user's Library folder, for easier access. */
+static void create_user_data_folder(pid_t pid)
+{
+    struct proc_bsdinfo proc;
+    proc_pidinfo(pid, PROC_PIDTBSDINFO, 0, &proc, sizeof(proc));
+
+    char path[USER_DATA_PATH_MAX];
+    sprintf(path, USER_DATA_ROOT "/%d", proc.pbi_uid);
+
+    if (mkdir(path, 0755)) return; // Might already exist
+    chown(path, proc.pbi_uid, proc.pbi_gid);
+
+    struct passwd *pw = getpwuid(proc.pbi_uid);
+    char library_path[PATH_MAX];
+    if (snprintf(library_path, sizeof(library_path), "%s/Library/MIP", pw->pw_dir) < sizeof(library_path)) {
+        symlink(path, library_path);
+        lchown(library_path, proc.pbi_uid, proc.pbi_gid);
+    }
+}
+
+static void handleClientMessageHook_common(uint64_t command, xpc_object_t dict)
+{
+    /* Command 500 is sent by all GUI processes to launchservicesd when they launch.
+       The process will block until it receives an answer from launchservicesd.
+       We will inject our code before we send a reply. Once we send the actual reply,
+       by calling the original method, the process resumes, runs the injected dlopen,
+       and continues normal execution. */
+
+    if (command == 500) {
+        audit_token_t token;
+        xpc_dictionary_get_audit_token(dict, &token);
+        pid_t pid = audit_token_to_pid(token);
+
+        /* While strictly speaking this should be loader's responsibility to create this folder,
+           this function must run as root, so it is done by the injector. */
+        create_user_data_folder(pid);
+
+        /* Before 10.12, the xpc dict also included a apptaskport key with task port send rights.
+           In 10.11 this could be considered a vulnerability - if you managed to receive this
+           xpc message, by setting up a fake launchservicesd daemon or managing to inject code to
+           the real one, you could completely bypass the need for task_for_pid and get a task port
+           for rootless/SIP-protected processes, something that wouldn't be possible under SIP
+           even as root. Not sure if this possible vulnerability is the reason Apple removed this
+           key, it could be just some code cleanup.
+         */
+        mach_port_t task = xpc_dictionary_copy_mach_send(dict, "apptaskport");
+
+        if (!task) {
+            task_for_pid(mach_task_self(), pid, &task);
+        }
+        inject_to_task(task, "/Library/Apple/System/Library/Frameworks/mip/loader.dylib");
+    }
+}
+
+static int64_t (*old_xpc_dictionary_get_int64)(xpc_object_t, const char *);
+static int64_t new_xpc_dictionary_get_int64(xpc_object_t dict, const char *key)
+{
+    int64_t ret = old_xpc_dictionary_get_int64(dict, key);
+    if (strcmp(key, "command") == 0) {
+        handleClientMessageHook_common(ret, dict);
+    }
+    return ret;
+}
+
+static void load_launchservicesd(void)
+{
+    MSHookFunction(xpc_dictionary_get_int64, new_xpc_dictionary_get_int64, (void**)&old_xpc_dictionary_get_int64);
 }
 
 bool should_inject_bundle(NSBundle *tweakBundle,
@@ -84,6 +162,11 @@ static void __attribute__((constructor)) loader(void)
             NSString *executable_name = @(executable_path).lastPathComponent;
             NSArray *disabled_bundles = user_preferences[@"MIPDisabledBundles"];
             NSLog(@"MIP: load %@", executable_name);
+
+            if ([@"launchservicesd" isEqualToString:executable_name]) {
+                load_launchservicesd();
+                return;
+            }
             
             // Enumerate tweak bundles and determine whether or not to load each
             for (NSURL *bundle_url in tweakBundles) {
