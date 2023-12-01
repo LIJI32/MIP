96753d9a865c688542c2815719ec81f654fd13cb
Lior Halphon <LIJI32@gmail.com>
2023-12-01 14:15:45 +0200

    Remove the old hooking code that used CoreSymbolication and use the Big Sur hook on all versions
    The old code introduced an Objective-C dependency to lsdinjector, which on unfortunate timings could crash launchservicesd upon injection – injection of Objective-C images while inside libobjc's `map_images` could cause a recursive lock crash. Fixes #17

--- MIP/injector/hook/thread_locking.h
+++
@@ -1,5 +0,0 @@
-/* Makes unsafe code thread-safe by pausing all other threads*/
-#include <mach/kern_return.h>
-
-kern_return_t suspend_all_other_threads(void);
-kern_return_t resume_all_other_threads(void);
--- MIP/injector/lsd_injector.c
+++ MIP/injector/lsd_injector.c
@@ -9,8 +9,6 @@
 #import <mach-o/loader.h>
 #import <mach-o/dyld.h>
 #include "inject/inject.h"
-#include "hook/hook.h"
-#include "hook/symbols.h"
 
 // Private APIs
 extern mach_port_t xpc_dictionary_copy_mach_send(xpc_object_t, const char *);
@@ -82,20 +80,7 @@ void handleClientMessageHook_common(uint64_t command, xpc_object_t dict)
     }
 }
 
-uint64_t handleClientMessageHook_Sierra(void *this, uint64_t command, xpc_object_t dict)
-{
-    handleClientMessageHook_common(command, dict);
-    return handleClientMessageOrig_Sierra(this, command, dict);
-}
-
-uint64_t handleClientMessageHook_HighSierra(void *this, void *session, xpc_connection_t connection, xpc_object_t dict)
-{
-    uint64_t command = xpc_dictionary_get_int64(dict, "command");
-    handleClientMessageHook_common(command, dict);
-    return handleClientMessageOrig_HighSierra(this, session, connection, dict);
-}
-
-uint64_t xpc_dictionary_get_int64_hook_BigSur(xpc_object_t dict, const char *key)
+uint64_t xpc_dictionary_get_int64_hook(xpc_object_t dict, const char *key)
 {
     uint64_t ret = xpc_dictionary_get_int64(dict, key);
     if (strcmp(key, "command") == 0) {
@@ -106,55 +91,39 @@ uint64_t xpc_dictionary_get_int64_hook_BigSur(xpc_object_t dict, const char *key
 
 void __attribute__((constructor)) hook_lsd(void)
 {
-#ifndef __arm64__
-    void *symbol = get_symbol("LSXPCClientConnection::handleClientMessage(unsigned long long, void*)");
-    if (symbol) {
-        handleClientMessageOrig_Sierra = hook_function(symbol, (void *) handleClientMessageHook_Sierra);
-    }
-    else {
-        symbol = get_symbol("LSXPCClient::handleClientMessage(__LSSession*, _xpc_connection_s*, void*)");
-        if (symbol) {
-            handleClientMessageOrig_HighSierra = hook_function(symbol, (void *) handleClientMessageHook_HighSierra);
-        }
-        else {
-#endif
-            struct mach_header_64 *header = (typeof (header))_dyld_get_image_header(0);
-            const struct load_command *cmd = (typeof(cmd))(header + 1);
-            const struct segment_command_64 *first = NULL;
-            const struct segment_command_64 *data = NULL;
-            for (unsigned i = 0; i < header->ncmds; i++, cmd = (typeof(cmd)) ((char*) cmd + cmd->cmdsize)) {
-                if (cmd->cmd == LC_SEGMENT_64) {
-                    if (!first && ((typeof(first))cmd)->filesize ) {
-                        first = (typeof(first)) cmd;
-                    }
-#ifndef __arm64__
-                    if (strcmp(((typeof(data))cmd)->segname, "__DATA") == 0) {
-#else
-                    if (strcmp(((typeof(data))cmd)->segname, "__DATA_CONST") == 0) {
-#endif
-                        data = (typeof(data))cmd;
-                        break;
-                    }
-                }
+    struct mach_header_64 *header = (typeof (header))_dyld_get_image_header(0);
+    const struct load_command *cmd = (typeof(cmd))(header + 1);
+    const struct segment_command_64 *first = NULL;
+    const struct segment_command_64 *data = NULL;
+    for (unsigned i = 0; i < header->ncmds; i++, cmd = (typeof(cmd)) ((char*) cmd + cmd->cmdsize)) {
+        if (cmd->cmd == LC_SEGMENT_64) {
+            if (!first && ((typeof(first))cmd)->filesize ) {
+                first = (typeof(first)) cmd;
             }
-            uintptr_t slide = (uintptr_t)header - first->vmaddr;
-            void **address = (void **)(data->vmaddr + slide);
-            void **end = (void **)(data->vmaddr + data->vmsize + slide);
-            while (address < end) {
-#if __arm64__
-                if (((uintptr_t)*address & 0xFFFFFFFFFFF) == ((uintptr_t)(xpc_dictionary_get_int64) & 0xFFFFFFFFFFF)) {
-                    mprotect((void *)(((uintptr_t)address) & ~0x3FFF), 0x4000, PROT_READ | PROT_WRITE);
-                    *address = ptrauth_sign_unauthenticated((void *)((uintptr_t)&xpc_dictionary_get_int64_hook_BigSur & 0xFFFFFFFFFFF), ptrauth_key_function_pointer, address);
-                }
-#else
-                if (*address == xpc_dictionary_get_int64) {
-                    *address = xpc_dictionary_get_int64_hook_BigSur;
-                }
-#endif
-                address++;
+    #ifndef __arm64__
+            if (strcmp(((typeof(data))cmd)->segname, "__DATA") == 0) {
+    #else
+            if (strcmp(((typeof(data))cmd)->segname, "__DATA_CONST") == 0) {
+    #endif
+                data = (typeof(data))cmd;
+                break;
             }
-#ifndef __arm64__
         }
     }
-#endif
+    uintptr_t slide = (uintptr_t)header - first->vmaddr;
+    void **address = (void **)(data->vmaddr + slide);
+    void **end = (void **)(data->vmaddr + data->vmsize + slide);
+    while (address < end) {
+    #if __arm64__
+        if (((uintptr_t)*address & 0xFFFFFFFFFFF) == ((uintptr_t)(xpc_dictionary_get_int64) & 0xFFFFFFFFFFF)) {
+            mprotect((void *)(((uintptr_t)address) & ~0x3FFF), 0x4000, PROT_READ | PROT_WRITE);
+            *address = ptrauth_sign_unauthenticated((void *)((uintptr_t)&xpc_dictionary_get_int64_hook & 0xFFFFFFFFFFF), ptrauth_key_function_pointer, address);
+        }
+    #else
+        if (*address == xpc_dictionary_get_int64) {
+            *address = xpc_dictionary_get_int64_hook;
+        }
+    #endif
+        address++;
+    }
 }
